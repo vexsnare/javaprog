cdb41b3cb034fac0b608135d86ebc9bc29b9182a
Vinay Saini <vinayknp.iiita@gmail.com>
2024-05-24 18:40:51 +0530

    Added print

--- src/main/java/psds/RodCutting.java
+++ src/main/java/psds/RodCutting.java
@@ -4,4 +4,7 @@ package psds;
  * @author author_name (vinay.saini@thoughtspot.com)
  */
 public class RodCutting {
+    public static void main(String[] args) {
+        System.out.printf("");
+    }
 }
