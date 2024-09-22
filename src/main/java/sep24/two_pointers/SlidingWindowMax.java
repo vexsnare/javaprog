b4e7fe963d1f0914d7ab7877ba44105f8ce4bc7e
Vinay Saini <vinayknp.iiita@gmail.com>
2024-09-22 16:44:59 +0530

    Update SlidingWindowMax.java

--- src/main/java/sep24/two_pointers/SlidingWindowMax.java
+++ src/main/java/sep24/two_pointers/SlidingWindowMax.java
@@ -1,4 +1,4 @@
-package psds.twoptr;
+package sep24.two_pointers;
 
 import utils.Printer;
 
@@ -6,15 +6,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * @author vinay.saini
- * You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
- * You can only see the k numbers in the window. Each time the sliding window moves right by one position.
- * Return the max sliding window
- * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
- * Output: [3,3,5,5,6,7]
+ * @see https://leetcode.com/problems/sliding-window-maximum/
  */
-
-// https://leetcode.com/problems/sliding-window-maximum/
+//
 
 public class SlidingWindowMax {
     public List<Integer> slidingMaximum(final List<Integer> num, int K) {
